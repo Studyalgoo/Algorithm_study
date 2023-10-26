cb704fecc0dcad1372c3135eeef21ae99cd451e6
yogain0416 <chiwoo416@naver.com>
2023-10-26 12:11:47 +0900

    week1_김민찬_11000수정

--- week1/김민찬/11000_강의실배정_김민찬.cpp
+++ week1/김민찬/11000_강의실배정_김민찬.cpp
@@ -1,60 +1,34 @@
-﻿#include <iostream>
+#include <iostream>
+#include <queue>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int N, cnt;
-bool room[200000];
-pair<int, int> p[200000];
-
-void GetClassRoom(int now)
-{
-	if (room[now])
-		return;
-
-	// 방문한 방 체크
-	room[now] = true;
-
-	for (int i = now + 1; i < N; i++)
-	{
-		// 이미 방문한 방이라면 패스
-		if (room[i])
-			continue;
-
-		// 수업 마치는 시간과 시작하는 시간이 같거나 시작하는 시간이 뒤에 있을 경우
-		if (p[now].second <= p[i].first)
-		{
-			GetClassRoom(i);
-			break;
-		}
-	}
-}
+int N, S, T;
+priority_queue<int, vector<int>, greater<int>> pq;
+vector<pair<int, int>> v;
 
 int main()
 {
 	cin >> N;
 
-	int S, T;
-
 	for (int i = 0; i < N; i++)
 	{
 		cin >> S >> T;
-		p[i] = make_pair(S, T);
+		v.push_back(make_pair(S, T));
 	}
 
-	// 시간 순서로 정렬
-	sort(p, p + N);
+	sort(v.begin(), v.end());
 
-	// 방 개수
-	cnt = 0;
-	for (int i = 0; i < N; i++)
+	for (pair<int, int> p : v)
 	{
-		// 방문한 방이 아닐 경우에만 cnt +1
-		if (room[i] == false)
-			cnt++;
-		GetClassRoom(i);
-	}
+		pq.push(p.second);
 
-	cout << cnt;
+		if (p.first >= pq.top())
+			pq.pop();
+	}
+	
+	cout << pq.size();
 
 	return 0;
 }
