fb05c666ab2d14711ee35c2df44062ebed4610f1
taetaerin <taerin13@naver.com>
2023-11-07 01:27:47 +0900

    week3

--- week3/김태린/1629_곱셈_김태린.js
+++ week3/김태린/1629_곱셈_김태린.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 let [a, b, c] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(BigInt);
 
+
+//분할 정복 알고리즘 사용
 const pow = (a, b, c) => {
   //b가 1이면 바로 c로 나누기
   if (b === BigInt(1)) {
@@ -9,12 +11,12 @@ const pow = (a, b, c) => {
   }
   const halfPow = pow(a, BigInt(parseInt(b / BigInt(2))), c);
 
-  //짝수일때
+  //짝수일때 
   if (b % BigInt(2) === BigInt(0)) {
     return (halfPow * halfPow) % c;
 
   } 
-  //홀수일때
+  //홀수일때 - a를 한번 더 곱해주기 
   else {
     return ((halfPow * halfPow * a) % c);
   }
